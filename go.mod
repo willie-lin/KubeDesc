e75b7f2211c4ff4a92256b8335a8b15a668e9120
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2020-04-15 13:03:40 +0800

    add errors

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module KubeDesc
 go 1.14
 
 require (
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96
 	github.com/imdario/mergo v0.3.9 // indirect
+	github.com/spf13/pflag v1.0.5
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
-	k8s.io/api v0.18.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.5.0
+	gopkg.in/yaml.v2 v2.2.8
+	k8s.io/api v0.18.0
 	k8s.io/apimachinery v0.18.0
 	k8s.io/client-go v0.18.0
 	k8s.io/utils v0.0.0-20200327001022-6496210b90e8 // indirect
 )
-
--- src/auth/kubeconfig.go
+++ src/auth/kubeconfig.go
@@ -1,5 +1,13 @@
 package auth
 
+import (
+	authApi "KubeDesc/src/auth/api"
+	"gopkg.in/yaml.v2"
+
+	_ "gopkg.in/yaml.v2"
+	"k8s.io/client-go/tools/clientcmd/api"
+)
+
 type contextInfo struct {
 	User string `yaml:"user"`
 }
@@ -7,4 +15,63 @@ type contextInfo struct {
 type contextEntry struct {
 	Name    string      `yaml:"name"`
 	Context contextInfo `yaml:"context"`
+}
+
+type userEntry struct {
+	Name string   `yaml:"name"`
+	User userInfo `yaml:"user"`
+}
+
+type authProviderConfig struct {
+	AccessToken string `yaml:"access-token"`
+}
+
+type authProviderInfo struct {
+	Config authProviderConfig `yaml:"config"`
+}
+
+type userInfo struct {
+	AuthProvider authProviderInfo `yaml:"auth-provider"`
+	Token        string           `yaml:"token"`
+	Username     string           `yaml:"username"`
+	Password     string           `yaml:"password"`
+}
+
+type kubeConfig struct {
+	Contexts       []contextEntry `yaml:"contexts"`
+	CurrentContext string         `yaml:"current-context"`
+	Users          []userEntry    `yaml:"users"`
+}
+type kubeConfigAuthenticator struct {
+	fileContent []byte
+	authModes   authApi.AuthenticationModes
+}
+
+func (self *kubeConfigAuthenticator) GetAuthInfo() (*api.AuthInfo, error) {
+	kubeConfig, err := self.parseKubeConfig(self.fileContent)
+
+}
+
+func (self *kubeConfigAuthenticator) parseKubeConfig(bytes []byte) (*kubeConfig, error) {
+	kubeConfig := new(kubeConfig)
+	if err := yaml.Unmarshal(bytes, kubeConfig); err != nil {
+		return nil, err
+	}
+	return kubeConfig, nil
+}
+
+func (self *kubeConfigAuthenticator) getCurrentUserInfo(config kubeConfig) (userInfo, error) {
+	userName := ""
+	for _, context := range config.Contexts {
+		if context.Name == config.CurrentContext {
+			userName = context.Context.User
+		}
+	}
+	if len(userName) == 0 {
+		return userInfo{}, errors.NewInvalid
+	}
+
+	for _, user := range config.Users {
+
+	}
 }
