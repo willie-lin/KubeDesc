7586e69ef8d521e3d59243c5cd2006bffe84d735
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2020-04-16 10:22:44 +0800

    add errors

--- src/errors/errors.go
+++ src/errors/errors.go
@@ -1,8 +1,10 @@
 package errors
 
 import (
+	"fmt"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"net/http"
 )
 
@@ -47,4 +49,98 @@ func NewNotFound(reason string) *errors.StatusError {
 			Message: reason,
 		},
 	}
+}
+
+func NewInternal(reason string) *errors.StatusError {
+	return &errors.StatusError{ErrStatus: metav1.Status{
+		Status: metav1.StatusFailure,
+		Code:   http.StatusInternalServerError,
+		Reason: metav1.StatusReasonInternalError,
+		Details: &metav1.StatusDetails{
+			Causes: []metav1.StatusCause{{Message: reason}},
+		},
+		Message: fmt.Sprintf("Internal error occurred: %s", reason),
+	}}
+}
+
+func NewUnexpectedObject(obj runtime.Object) *errors.StatusError {
+	return &errors.StatusError{
+		ErrStatus: metav1.Status{
+			Status:  metav1.StatusFailure,
+			Code:    http.StatusInternalServerError,
+			Reason:  metav1.StatusReasonInternalError,
+			Message: errors.FromObject(obj).Error(),
+		},
+	}
+}
+
+func NewGenericResponse(code int, serverMessage string) *errors.StatusError {
+	reason := metav1.StatusReasonUnknown
+	message := fmt.Sprintf("the server responded with the status code %d but did not return more information", code)
+	switch code {
+	case http.StatusNotFound:
+		reason = metav1.StatusReasonNotFound
+		message = "the server could not find the requested resource"
+	case http.StatusBadRequest:
+		reason = metav1.StatusReasonBadRequest
+		message = "the server rejected our request for an unknown reason."
+	case http.StatusUnauthorized:
+		reason = metav1.StatusReasonUnauthorized
+		message = "the server has asked for the client to provide credentials"
+	case http.StatusForbidden:
+		reason = metav1.StatusReasonForbidden
+		message = serverMessage
+	case http.StatusConflict:
+		reason = metav1.StatusReasonConflict
+		message = serverMessage
+	case http.StatusNotAcceptable:
+		reason = metav1.StatusReasonNotAcceptable
+		message = serverMessage
+	case http.StatusUnsupportedMediaType:
+		reason = metav1.StatusReasonUnsupportedMediaType
+		message = serverMessage
+	case http.StatusMethodNotAllowed:
+		reason = metav1.StatusReasonMethodNotAllowed
+		message = "the server does not allow this method on the requested resource"
+	case http.StatusUnprocessableEntity:
+		reason = metav1.StatusReasonInvalid
+		message = "the server rejected our request due to an error in our request"
+	case http.StatusServiceUnavailable:
+		reason = metav1.StatusReasonServiceUnavailable
+		message = "the server is currently unable to handle the request"
+	case http.StatusGatewayTimeout:
+		reason = metav1.StatusReasonTimeout
+		message = "the server was unable to return a response in the time allotted, but may still be processing the request"
+	case http.StatusTooManyRequests:
+		reason = metav1.StatusReasonTooManyRequests
+		message = "the server has received too many requests and has asked us to try again later"
+	default:
+		if code >= 500 {
+			reason = metav1.StatusReasonInternalError
+			message = fmt.Sprintf("an error on the server (%q) has prevented the request from succeeding", serverMessage)
+		}
+	}
+	return &errors.StatusError{
+		ErrStatus: metav1.Status{
+			Status:  metav1.StatusFailure,
+			Code:    int32(code),
+			Reason:  reason,
+			Message: message,
+		}}
+}
+
+func IsTokenExpired(err error) bool {
+	statusErr, ok := err.(*errors.StatusError)
+	if !ok {
+		return false
+	}
+	return statusErr.ErrStatus.Message == MsgTokenExpiredError
+}
+
+func IsAlreadyExists(err error) bool {
+	return errors.IsAlreadyExists(err)
+}
+
+func IsUnauthorized(err error) bool {
+	return errors.IsUnauthorized(err)
 }
